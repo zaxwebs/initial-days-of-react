d1a9b507da1ff2c8308143864625bc6ea879fe94
zaxwebs <zifidigital@gmail.com>
2021-10-21 08:30:42 +0530

    todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const initialTasks = [
+		{
+			title: "Learn the basics of React",
+		},
+		{
+			title: "Learn more about React",
+		},
+	]
+
+	const [tasks, setTasks] = useState([...initialTasks])
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		e.target.title.value &&
+			setTasks([...tasks, { title: e.target.title.value }])
+		//reset value
+		e.target.title.value = ""
+	}
+
+	const handleDelete = (index) => {
+		let newTasks = [...tasks]
+		newTasks.splice(index, 1)
+		setTasks(newTasks)
+	}
+
+	return (
+		<div className="container py-3">
+			<pre>
+				The objective of this repo is to understand state and events.
+				Dividing into components will be covered later.
+			</pre>
+			<form className="mb-3" onSubmit={handleSubmit}>
+				<div className="row">
+					<div className="col-md-8">
+						<h3 className="mb-3">Create a Task</h3>
+						<input
+							type="text"
+							className="form-control"
+							name="title"
+						></input>
+					</div>
+					<div className="col-md-4 d-flex align-items-end">
+						<button type="submit" className="btn btn-primary w-100">
+							Add Task
+						</button>
+					</div>
+				</div>
+			</form>
+			<h3 className="mb-3">Your Tasks</h3>
+			{!tasks.length && (
+				<div className="text-muted">
+					You don't have any tasks at the moment.
+				</div>
+			)}
+
+			<ul className="list-group">
+				{tasks.map((task, index) => (
+					<li key={index} className="list-group-item">
+						<div className="task d-flex">
+							<div className="flex-grow-1">{task.title}</div>
+							<div className="">
+								<button
+									type="submit"
+									className="btn btn-secondary btn-sm"
+									onClick={() => handleDelete(index)}
+								>
+									Delete Task
+								</button>
+							</div>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
-export default App;
+export default App
